e42afbeef9d653955e375d454d96f8755371d0d0
annie-bw <a.bwiza1@alustudent.com>
2025-10-19 22:54:04 +0200

    Refactor script to export TODO list to JSON format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
-"""Script that gets user data (Todo list) from API
-and then export the result to csv file. """
-
+"""
+Script that exports employee TODO list data to JSON format
+"""
 import json
 import requests
 import sys
 
 
-def main():
-    """main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
 
-    response = requests.get(todo_url)
-    user_name = requests.get(user_url).json().get('username')
-    user_data = []
-    output = {user_id: user_data}
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id)
 
-    for todo in response.json():
-        if todo.get('userId') == user_id:
-            user_data.append(
-                {
-                    "task": todo.get('title'),
-                    "completed": todo.get('completed'),
-                    "username": user_name,
-                })
-    print(output)
-    file_name = "{}.json".format(user_id)
-    with open(file_name, 'w') as file:
-        json.dump(output, file)
+    user = requests.get(url_user).json()
+    todos = requests.get(url_todos).json()
 
+    username = user.get("username")
 
-if __name__ == '__main__':
-    main()
+    tasks_list = []
+    for task in todos:
+        task_dict = {
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username
+        }
+        tasks_list.append(task_dict)
+
+    json_data = {employee_id: tasks_list}
+
+    filename = "{}.json".format(employee_id)
+    with open(filename, 'w') as json_file:
+        json.dump(json_data, json_file)
