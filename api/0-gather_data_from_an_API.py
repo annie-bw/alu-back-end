b3c3d6a3c0a1f83c2e87bf91e1331b7304bc2b3b
annie-bw <a.bwiza1@alustudent.com>
2025-10-19 22:50:20 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,31 +9,31 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
     except ValueError:
         sys.exit(1)
-    
+
     base_url = "https://jsonplaceholder.typicode.com"
-    
+
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    
+
     if user_response.status_code != 200 or todos_response.status_code != 200:
         sys.exit(1)
-    
+
     user_data = user_response.json()
     todos_data = todos_response.json()
-    
+
     employee_name = user_data.get("name")
-    
+
     completed_tasks = [task for task in todos_data if task.get("completed")]
     total_tasks = len(todos_data)
     number_of_done_tasks = len(completed_tasks)
-    
-    print(f"Employee {employee_name} is done with tasks"
-          f"({number_of_done_tasks}/{total_tasks}):")
-    
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_done_tasks, total_tasks))
+
     for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+        print("\t {}".format(task.get('title')))
