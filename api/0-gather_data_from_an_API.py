696918873a62271ed4e76bde8869ec19bdafe7ff
annie bwiza <a.bwiza1@laustudent.com>
2025-10-24 14:05:32 +0000

    add this final one

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,61 @@
 #!/usr/bin/python3
-"""Script to get todos for a user from API"""
+"""
+This module retrieves todos for a given user from the JSONPlaceholder API
+and displays the completed tasks along with the total number of tasks.
+"""
 
 import requests
 import sys
 
 
 def main():
-    """main function"""
-    user_id = int(sys.argv[1])
+    """
+    Main function to fetch and display a user's todos.
+
+    Usage:
+        python3 0-gather_data_from_an_API.py <user_id>
+    
+    Args:
+        user_id (int): The ID of the user to retrieve todos for.
+    """
+    try:
+        user_id = int(sys.argv[1])
+    except (IndexError, ValueError):
+        print("Usage: python3 0-gather_data_from_an_API.py <user_id>")
+        sys.exit(1)
+
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
 
+    # Get todos and user information
     response = requests.get(todo_url)
+    if response.status_code != 200:
+        print("Failed to fetch todos")
+        sys.exit(1)
+    todos = response.json()
 
-    total_questions = 0
-    completed = []
-    for todo in response.json():
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Failed to fetch user information")
+        sys.exit(1)
+    user_name = user_response.json().get('name', 'Unknown')
 
-        if todo['userId'] == user_id:
-            total_questions += 1
+    # Filter todos for the specified user
+    total_tasks = 0
+    completed_tasks = []
 
-            if todo['completed']:
-                completed.append(todo['title'])
+    for todo in todos:
+        if todo.get('userId') == user_id:
+            total_tasks += 1
+            if todo.get('completed'):
+                completed_tasks.append(todo.get('title'))
 
-    user_name = requests.get(user_url).json()['name']
-
-    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
-               len(completed), total_questions))
-    print(printer)
-    for q in completed:
-        print("\t {}".format(q))
+    # Print results
+    print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task}")
 
 
 if __name__ == '__main__':
     main()
-        i
+
