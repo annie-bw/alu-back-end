d0f8681e392ab589abeaafb84d465bab8717d895
annie bwiza <a.bwiza1@laustudent.com>
2025-10-25 07:07:48 +0000

    add this final one

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,25 +5,33 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
+def main():
+    """main function"""
     user_id = int(sys.argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 
-    user_response = requests.get(user_url)
-    user_name = user_response.json().get("name")
+    response = requests.get(todo_url)
 
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
+    total_questions = 0
+    completed = []
+    for todo in response.json():
 
-    total_tasks = 0
-    done_tasks = []
+        if todo['userId'] == user_id:
+            total_questions += 1
 
-    for todo in todos:
-        if todo.get("userId") == user_id:
-            total_tasks += 1
-            if todo.get("completed"):
-                done_tasks.append(todo.get("title"))
+            if todo['completed']:
+                completed.append(todo['title'])
 
-   
+    user_name = requests.get(user_url).json()['name']
+
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
+    for q in completed:
+        print("\t {}".format(q))
+
+
+if __name__ == '__main__':
+    main()
 
